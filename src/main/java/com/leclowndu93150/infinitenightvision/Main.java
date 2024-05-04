78fead416aeff9cd9d29141ec32e8a725590b4be
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-05-04 22:06:49 +0200

    Port to 1.20.5

---
+++ src/main/java/com/leclowndu93150/infinitenightvision/Main.java
@@ -0,0 +1,33 @@
+package com.leclowndu93150.infinitenightvision;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Mob;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+
+@Mod(Main.MODID)
+public class Main
+{
+    public static final String MODID = "infinitenightvision";
+
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientTick(ClientTickEvent.Post event) {
+            Minecraft minecraft = Minecraft.getInstance();
+            if (minecraft.player != null) {
+                if (!minecraft.player.hasEffect(MobEffects.NIGHT_VISION)) {
+                    Minecraft.getInstance().player.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION,-1,1));
+                }
+            }
+
+
+        }
+    }
+}
